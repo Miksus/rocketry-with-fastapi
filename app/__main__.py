dddd685f7c3510595765a2a9ae2dacebf4f8ecb3
Mikael Koli <koli.mikael@gmail.com>
2022-12-12 08:38:22 +0200

    upd: removed unneeded CLI args

--- app/__main__.py
+++ app/__main__.py
@@ -4,10 +4,7 @@ from app.main import main
 def cli(args=None):
     parser = argparse.ArgumentParser(description='Run API and scheduler.')
 
-    parser.add_argument('--api', action='store_true', help="Start the API. (default)")
     parser.add_argument('--no-api', dest='fastapi', action='store_false', help="Don't start the API.")
-
-    parser.add_argument('--scheduler', action='store_true', help="Start the Scheduler. (default)")
     parser.add_argument('--no-scheduler', dest='rocketry', action='store_false', help="Don't start the scheduler.")
 
     parser.set_defaults(fastapi=True, rocketry=True)
